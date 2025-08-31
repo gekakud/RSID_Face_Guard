1217de93b78b9792be43f63472f93a115362d5a2
Evgeny P <geka.kud@gmail.com>
2025-08-31 19:58:49 +0300

    add led test

---
+++ led_test.py
@@ -0,0 +1,37 @@
+import time, board, neopixel
+
+NUM_LEDS = 19
+pixels = neopixel.NeoPixel(
+    board.D21, NUM_LEDS,
+    brightness=0.6,
+    auto_write=False,  
+)
+
+def all_off():
+    pixels.fill((0,0,0))
+    pixels.show()
+
+all_off()
+
+
+
+for i in range(0,6):
+    pixels[i] = (0, 255, 0)
+
+for i in range(13,19):
+    pixels[i] = (0, 255, 0)
+
+pixels.show()
+
+time.sleep(2)
+for i in range(0,6):
+    pixels[i] = (255, 0, 0)
+
+for i in range(13,19):
+    pixels[i] = (255, 0, 0)
+
+pixels.show()
+time.sleep(2)
+
+all_off()
+
